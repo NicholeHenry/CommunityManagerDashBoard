c42152e8d235178e8a8c24d139adcee12d845ec9
Nichole Henry <nichole.henry12@gmail.com>
2019-08-15 11:43:41 -0500

    working on setting up twilio

---
+++ CommunityManagerDashBoard/Controllers/SmsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+namespace CommunityManagerDashBoard.Controllers
+{
+    public class SmsController : Controller
+    {
+        public IActionResult Index(SmsRequest incomingMessage)
+        {
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message("The Copy cat says:" + incomingMessage.Body);
+            return TwiML(messagingResponse);
+        }
+    }
+}
